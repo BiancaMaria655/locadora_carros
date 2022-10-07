70160feb956329563d2f94145ab06934186acf60
DaviMSilva <davisilvameneses@gmail.com>
2022-10-06 23:00:10 -0300

    criação de pastas e index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3333
+
+
+
+
+
+app.listen(port)
