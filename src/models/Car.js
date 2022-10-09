7f483c0dc45b4cee6202af6c4c9b0741ace7fa19
Francisco Ribeiro <104844729+bakuryn@users.noreply.github.com>
2022-10-09 14:49:50 -0300

    model de Carros criado

---
+++ src/models/Car.js
@@ -0,0 +1,48 @@
+const Sequelize = require('sequelize');
+const db = require('./Database');
+
+const Car = db.define('cars', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  modelo: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  ano: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  fabricante: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  valor_loc: {
+    type: Sequelize.DECIMAL(10,2),
+    allowNull: false
+  },
+  cor: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  disponivel: {
+    type: Sequelize.STRING,
+    allowNull: false
+    // S ou N
+  },
+  adicionais: {
+    type: Sequelize.STRING,
+    allowNull: true
+  }
+});
+
+Car.sync();
+
+module.exports = Car;
