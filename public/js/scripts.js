99de8f86b9209d62e0ac4264a1b8a5ec595e2153
BiancaMaria655 <biancasouza.maria@gmail.com>
2022-10-12 21:05:26 -0300

    inserindo mascaras no customerRegistrationn

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,29 +1,29 @@
 function editFields(list) {
-  var el = document.getElementById(list);
-  let elId = el.addEventListener('click', function (e) {
-    console.log(e.target.id);
-    elId = e.target.id;
+    var el = document.getElementById(list);
+    let elId = el.addEventListener('click', function(e) {
+        console.log(e.target.id);
+        elId = e.target.id;
 
-    var btn = document.getElementById(elId);
-    var id = btn.id.slice(4)
-    var btnS = document.getElementById('btnS'+id)
-    var btnE = document.getElementById('btnE'+id)
+        var btn = document.getElementById(elId);
+        var id = btn.id.slice(4)
+        var btnS = document.getElementById('btnS' + id)
+        var btnE = document.getElementById('btnE' + id)
 
-    
-    if ((btnS.id === elId) || (btnE.id === elId)) {
-        
-        
-        btnE.toggleAttribute('hidden');
-        btnS.toggleAttribute('hidden')
 
-        var name = document.getElementById('name'+id)
-        name.toggleAttribute('disabled')
-      }
-      
-      
-    
-  });
-  
+        if ((btnS.id === elId) || (btnE.id === elId)) {
+
+
+            btnE.toggleAttribute('hidden');
+            btnS.toggleAttribute('hidden')
+
+            var name = document.getElementById('name' + id)
+            name.toggleAttribute('disabled')
+        }
+
+
+
+    });
+
 }
 
 editFields('adminList')
