ea9d1f50425577c59f1fe53c26be83403b219444
Francisco Ribeiro <104844729+bakuryn@users.noreply.github.com>
2022-10-15 17:38:51 -0300

    msg adicionada no model car

--- models/Car.js
+++ models/Car.js
@@ -10,28 +10,47 @@ const Car = db.define('cars', {
   },
   nome: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    notEmpty: {
+      msg: 'Nome não pode ser em Branco!'
+    }
   },
 
   modelo: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    notEmpty: {
+      msg: 'Modelo não pode ser em Branco!'
+    }
   },
   ano: {
     type: Sequelize.INTEGER,
-    allowNull: false
+    allowNull: false,
+    notEmpty: {
+        msg: 'Ano não pode ser em branco!'
+    }
   },
   fabricante: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    notEmpty: {
+      msg: 'Fabricante não pode ser em branco!'
+    }
   },
   valor_loc: {
     type: Sequelize.DECIMAL(10,2),
-    allowNull: false
+    allowNull: false,
+    notEmpty: {
+        msg: 'Valore da locação não pode ser em branco!'
+    
+    }
   },
   cor: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    notEmpty: {
+      msg: 'Nome não pode ser em Branco!'
+    }
   },
   disponivel: {
     type: Sequelize.STRING,
